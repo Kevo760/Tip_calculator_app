ef4943e1c87513efd36a664fda50090d2d5e5ec9
TheLegend760 <73809530+TheLegend760@users.noreply.github.com>
2022-01-21 02:41:41 -0700

    Beta v1.2
    -Change classes
    -Changed a few style
    -Added more script

--- script.js
+++ script.js
@@ -1,2 +1,45 @@
 let billTotal = document.getElementById("bill_total").value;
+let numPeps = document.getElementById("num_peps");
+
+// Tips Section
 let fivePercent = document.getElementById("five_percent").value;
+let tenPercent = document.getElementById("ten_percent").value;
+let fifthteenPercent = document.getElementById("fifthteen_percent").value;
+let twentyFivePercent = document.getElementById("twenty_five_percent").value;
+let fiftyPercent = document.getElementById("fifty_percent").value;
+let customTip = document.getElementById("custom_tip").value;
+
+// Totals
+let tipAmount = document.getElementById("tip_amount");
+let totalAmount = document.getElementById("total_amount");
+
+// Tip amount
+let tipTotal = fivePercent * billTotal;
+
+
+function resetToZero() {
+    billTotal.value = "";
+    numPeps.value = "";
+    customTip.value = "";
+    tipAmount.innerText = "$0.00";
+    totalAmount.innerText = "$0.00";
+    zero.classList.remove("error");
+}
+
+function checkZero() {
+    let zero = document.getElementById("zero");
+    if (numPeps.value == 0) {
+        zero.classList.add("error")
+    } else {
+        zero.classList.remove("error")
+    }
+}
+
+
+
+console.log(tipTotal)
+
+
+
+
+
