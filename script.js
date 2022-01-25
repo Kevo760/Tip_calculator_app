36d5619223573b3bbad97e67f6f9a5ea628aa21e
TheLegend760 <73809530+TheLegend760@users.noreply.github.com>
2022-01-25 01:43:53 -0700

    Final v1.0.0
    -Fixed Up styling
    -Added onchange events
    -Added additional javascript functions
    -Deleted unused java objects

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ let billTotal = document.getElementById("bill_total");
 let numPeps = document.getElementById("num_peps");
 
 // Tips Section
-let fivePercent = document.getElementById("five_percent");
-let tenPercent = document.getElementById("ten_percent");
-let fifthteenPercent = document.getElementById("fifthteen_percent");
-let twentyFivePercent = document.getElementById("twenty_five_percent");
-let fiftyPercent = document.getElementById("fifty_percent");
-let customTip = document.getElementById("custom_tip");
+let other = document.getElementById('other');
+let tips = document.getElementsByName('tips');
+let customTip = document.getElementById('custom_tip');
+
+
 
 
 
@@ -24,10 +23,11 @@ function resetToZero() {
     tipAmount.innerText = "$0.00";
     totalAmount.innerText = "$0.00";
     zero.classList.remove("error");
+    tips[0].checked=true;
 }
 
 
-// Adds .error class on num of peeople
+// Adds .error class on num of people
 function checkZero() {
     let zero = document.getElementById("zero");
     if (numPeps.value == 0) {
@@ -49,28 +49,82 @@ function otherTip(){
     a.value="";
     }
 
-
-
-
 // if bill equals != "" && num of peps !0 && tip
 
 
-let form = document.querySelector('input[type = radio]:checked').value;
 
 
+// function to convert selected tip to 1/100 of a decimal
+function tipConversion() {
+for (var i = 0; i < tips.length; i++) {   
+    if (tips[i].checked) {
+        other.value = customTip.value;
+        return tips[i].value * .01;
+   }
 
-// TEST
-let test = document.getElementById("test");
+}
+}
 
-function testTip() {
-//    let convertTip = customTip.value * .01;
-//    let testTipv1 = convertTip * billTotal.value;
-//    return test.textContent=testTipv1.toFixed(2);
-return alert(form);
+//Calculates tips per person
+function tipPerPerson() {
+    let tip = tipConversion();
+
+    if (billTotal.value == '') {
+        billTotal.value === 0;
+    } else if (numPeps.value == '') {
+        numPeps.value === 0;
+    } else {
+        let totalTip = parseFloat(billTotal.value) * tip;
+        let TipPerPerson =  totalTip / numPeps.value;
+        return TipPerPerson.toFixed(2);
+    }
+
 }
 
 
+//Calculates bill total with tips per person
+function totalPerPerson() {
 
+    let tipsPerP = tipPerPerson();
+
+    
+     if (billTotal.value == '') {
+        billTotal.value === 0;
+    } else if (numPeps.value == '') {
+        numPeps.value === 0;
+    } else if (tipsPerP == undefined) {
+        tipsPerP === 0;
+    } else {
+        let billPerP = billTotal.value / numPeps.value;
+        let tipsBillPerPerson = parseFloat(tipsPerP) + billPerP;
+        return tipsBillPerPerson.toFixed(2);
+    }
+
+}
+
+
+// calculates both tipsPerPerson and TotalPerPerson Function
+function calculate() {
+    let tipPP = tipPerPerson();
+    let totalPP = totalPerPerson();
+
+    if (tipPP == undefined) {
+        tipPP == 0.00;
+    } else if (totalPP == undefined) {
+        totalPP == 0.00
+    } else {
+    tipAmount.innerText = "$" + tipPP;
+    totalAmount.innerText = "$" + totalPP;
+    }
+
+}
+
+
+// recalculates when selecting different tip amount and sets custom tip to no value
+function radioChange() {
+    calculate();
+return customTip.value = "";
+}
 
 
 
